4648c7cb163cf2753e62e9f9309c0944f444caad
unknown <0709.marcoscouto@gmail.com>
2023-06-07 15:33:08 -0300

    Update server URL

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async(e) => {
   loader(messageDiv)
 
   // Fetch data from server -> bot's response
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://openai-application.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
